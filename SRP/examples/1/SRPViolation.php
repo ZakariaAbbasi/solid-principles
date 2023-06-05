f70abef16b9fc620f5c180dab200aa67f903aa18
Zakaria Abbasi <zakaria.abbasi75@gmail.com>
2023-06-05 21:49:53 +0330

    added example one srp

--- SRP/examples/1/SRPViolation.php
+++ SRP/examples/1/SRPViolation.php
@@ -1,6 +1,25 @@
 <?php
 
-class SRPViolation
-{
 
+class Modem
+{
+    public function dial($pno)
+    {
+        // Implementing dial($pno) method.
+    }
+
+    public function hangup()
+    {
+        //  Implementing hangup() method.
+    }
+
+    public function send($c)
+    {
+        // Implementing send($c) method.
+    }
+
+    public function receive()
+    {
+        // Implementing receive() method.
+    }
 }
